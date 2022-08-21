3fe46a4855ff599442e86a474d66fc817adeee60
tho.nguyentien <tho.nguyentien@vndirect.com.vn>
2022-08-22 02:45:50 +0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # techmaster-ndc-javacore14
+
 Bài giảng thực hành và BTVN của lớp JAVA core 14
---
+++ src/test/java/nguyentientho/techmasterndcjavacore14/TechmasterNdcJavacore14ApplicationTests.java
@@ -0,0 +1,13 @@
+package nguyentientho.techmasterndcjavacore14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmasterNdcJavacore14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
